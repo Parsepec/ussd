05c52e3e6bdb73e686f3ea00754137642702faf2
parsepec <parsepecq@gmail.com>
2023-05-23 13:09:38 +0100

    Post handler

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Database, Table } from "npm:st.db";
+const db = new Database("db.json");
+const router = new Router();
+
+db.on("ready", () => {
+  const table_1 = new Table("profile", db);
+  table_1.set("name", "Mohamed Abdelkarim");
+  table_1.set("age", 17);
+});
 
 (async () => {
-  const app = new Application();
-
-  app.use(ctx => {
+  //   app.use((ctx) => {
+  //     ctx.response.body = "Hello World!";
+  //   });
+  router.post("/", async (ctx, next) => {
     ctx.response.body = "Hello World!";
+    console.log(ctx.request)
+    const { value } = ctx.request.body({ type: "json" });
+    const { name } = await value;
+    console.log(name);
   });
-
-  await app.listen("127.0.0.1:8000");
+  const app = new Application();
+  const port = 3000;
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+  app.addEventListener("listen", () => {
+    console.log(`Listening on localhost:${port}`);
+  });
+  await app.listen(`127.0.0.1:${port}`);
 })();
