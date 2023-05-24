ca0eec75908c0ab85daa0a923aa6af113239435e
parsepec <parsepecq@gmail.com>
2023-05-24 14:46:25 +0100

    To africas talking

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-// import { Database, Table } from "npm:st.db";
-// const db = new Database("db.json");
-// 
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { load } from "https://deno.land/std/dotenv/mod.ts";
 
-// db.on("ready", () => {
-//   const table_1 = new Table("profile", db);
-//   table_1.set("name", "Mohamed Abdelkarim");
-//   table_1.set("age", 17);
-// });
+const env = await load();
+const supabaseUrl = "https://recdhklrrpqdbxuanydk.supabase.co";
+const supabaseKey = env["SERVICE_KEY"];
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// session_id gets a unique session ID
+// service_code gets your USSD code
+// phone_number gets the phone number thats currently accessing the USSD
+// text carries the user response as they use the USSD application
 const router = new Router();
 (async () => {
   //   app.use((ctx) => {
@@ -15,11 +18,19 @@ const router = new Router();
   //   });
   router.post("/", async (ctx, next) => {
 
-    console.log(ctx.request)
-    const { value } = ctx.request.body({ type: "json" });
-    const { name } = await value;
-    console.log(name);
-    ctx.response.body = name;
+    console.log(ctx.request.url.searchParams.get("name"));
+    const params = ctx.request.url.searchParams;
+
+    params.forEach((value, key, parent) => {
+      console.log(value, key);
+    });
+
+    var text = params.get("text");
+    console.log({text})
+    if (Boolean(text == "")) {
+        console.log("If passed")
+        ctx.response.body = "CON Good day";
+      }
   });
   const app = new Application();
   const port = 3000;
